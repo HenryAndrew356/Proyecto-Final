10f72e2dab42459749dadb721b8bbf10d4c0d45e
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2020-06-15 19:06:29 -0500

    Confirmo Cambios.

---
+++ smflSetup/smflSetup/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!"); //
+    sf::CircleShape shape(225.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
