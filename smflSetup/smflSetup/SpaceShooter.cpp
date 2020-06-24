9811d127d3050f3a069d85c2c2bdd042caafe33b
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2020-06-24 07:16:57 -0500

    Confirmo cambios YYY

--- smflSetup/smflSetup/SpaceShooter.cpp
+++ smflSetup/smflSetup/SpaceShooter.cpp
@@ -1,111 +1,91 @@
-#include<iostream>			
+#include<iostream>
 #include"SFML\Graphics.hpp"	
 #include"SFML\Window.hpp"
 #include"SFML\System.hpp"
+#include"SFML\Audio.hpp"
 #include<math.h>
 #include<cstdlib>
 #include<vector>
 
 using namespace sf;
 
-class Bullet
-{
+class Bullet{
 public:
 	Sprite shape;
-
-	Bullet(Texture* texture, Vector2f pos)
-	{
+	Bullet(Texture* texture, Vector2f pos) {
 		this->shape.setTexture(*texture);
-
 		this->shape.setScale(0.07f, 0.07f);
-
 		this->shape.setPosition(pos);
 	}
-
 	~Bullet() {}
 };
 
-class Player
-{
+class Player{
 public:
 	Sprite shape;
 	Texture* texture;
-
 	int HP;
 	int HPMax;
-
 	std::vector<Bullet> bullets;
 
-	Player(Texture* texture)
-	{
+	Player(Texture* texture) {
 		this->HPMax = 5;
 		this->HP = this->HPMax;
-
 		this->texture = texture;
 		this->shape.setTexture(*texture);
-
 		this->shape.setScale(0.1f, 0.1f);
 	}
 	~Player() {}
 };
 
-class Enemy
-{
+class Enemy{
 public:
 	Sprite shape;
-
 	int HP;
 	int HPMax;
-
-	Enemy(Texture* texture, Vector2u windowSize)
-	{
+	Enemy(Texture* texture, Vector2u windowSize) {
 		this->HPMax = rand() % 3 + 1;
 		this->HP = this->HPMax;
-
 		this->shape.setTexture(*texture);
-
 		this->shape.setScale(0.1f, 0.1f);
-
 		this->shape.setPosition(windowSize.x - this->shape.getGlobalBounds().width, rand() % (int)(windowSize.y - this->shape.getGlobalBounds().height));
 	}
-
 	~Enemy() {}
 };
-int main()
-{
-	srand(time(NULL));
 
-	RenderWindow window(VideoMode(800, 600), "Spaceship action!", Style::Default);
+int main(){
+	srand(time(NULL));
+	RenderWindow window(VideoMode(800, 600), "Space Shooter", Style::Default);
 	window.setFramerateLimit(60);
+
+	Music music;
+	music.openFromFile("FondoGame.wav");
+	music.setVolume(50);
+	music.play();
+
 
 	//Init text
 	Font font;
 	font.loadFromFile("Fonts/Dosis-Light.ttf");
-
 	//Init textures
 	Texture playerTex;
 	playerTex.loadFromFile("Textures/ship.png");
-
 	Texture enemyTex;
 	enemyTex.loadFromFile("Textures/enemy.png");
-
 	Texture bulletTex;
-	bulletTex.loadFromFile("Textures/missileTex01.png");
-
+	bulletTex.loadFromFile("Textures/Blue_laser.png");
 	//UI init
 	Text scoreText;
 	scoreText.setFont(font);
 	scoreText.setCharacterSize(20);
 	scoreText.setFillColor(Color::White);
 	scoreText.setPosition(20.f, 20.f);
-
 	Text gameOverText;
 	gameOverText.setFont(font);
 	gameOverText.setCharacterSize(30);
 	gameOverText.setFillColor(Color::Red);
 	gameOverText.setPosition(100.f, window.getSize().y / 2);
 	gameOverText.setString("JUEGO TERMINADO");
-
 	//Player init
 	int score = 0;
 	Player player(&playerTex);
@@ -114,7 +94,6 @@ int main()
 	hpText.setFont(font);
 	hpText.setCharacterSize(12);
 	hpText.setFillColor(Color::White);
-
 	//Enemy init
 	int enemySpawnTimer = 0;
 	std::vector<Enemy> enemies;
@@ -123,19 +102,21 @@ int main()
 	eHpText.setFont(font);
 	eHpText.setCharacterSize(12);
 	eHpText.setFillColor(Color::White);
-
-	while (window.isOpen())
-	{
+	
+	while (window.isOpen()){
 		Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)){
 			if (event.type == Event::Closed)
 				window.close();
 		}
+		
 
-		if (player.HP > 0)
-		{
-			//Player
+		
+
+
+
+		if (player.HP > 0){
+			//Player manejo de movimientos
 			if (Keyboard::isKeyPressed(Keyboard::W))
 				player.shape.move(0.f, -10.f);
 			if (Keyboard::isKeyPressed(Keyboard::A))
@@ -148,107 +129,89 @@ int main()
 			hpText.setPosition(player.shape.getPosition().x, player.shape.getPosition().y - hpText.getGlobalBounds().height);
 			hpText.setString(std::to_string(player.HP) + "/" + std::to_string(player.HPMax));
 
-			//Collision with window
-			if (player.shape.getPosition().x <= 0) //Left
+			//Restricciones de movimiento de Player
+			if (player.shape.getPosition().x <= 0) //Izquierda
 				player.shape.setPosition(0.f, player.shape.getPosition().y);
-			if (player.shape.getPosition().x >= window.getSize().x - player.shape.getGlobalBounds().width) //Right
+			if (player.shape.getPosition().x >= window.getSize().x - player.shape.getGlobalBounds().width) //Derecha
 				player.shape.setPosition(window.getSize().x - player.shape.getGlobalBounds().width, player.shape.getPosition().y);
-			if (player.shape.getPosition().y <= 0) //Top
+			if (player.shape.getPosition().y <= 0) //Limite Superior
 				player.shape.setPosition(player.shape.getPosition().x, 0.f);
-			if (player.shape.getPosition().y >= window.getSize().y - player.shape.getGlobalBounds().height) //Bottom
+			if (player.shape.getPosition().y >= window.getSize().y - player.shape.getGlobalBounds().height) //Limite Inferior
 				player.shape.setPosition(player.shape.getPosition().x, window.getSize().y - player.shape.getGlobalBounds().height);
 
-			//Update Controls
+			//Controles de Actualizaion
 			if (shootTimer < 15)
 				shootTimer++;
 
-			if (Mouse::isButtonPressed(Mouse::Left) && shootTimer >= 15) //Shooting
-			{
+			if (Mouse::isButtonPressed(Mouse::Left) && shootTimer >= 15){ //Shooting
 				player.bullets.push_back(Bullet(&bulletTex, player.shape.getPosition()));
 				shootTimer = 0; //reset timer
 			}
 
-			//Bullets
-			for (size_t i = 0; i < player.bullets.size(); i++)
-			{
-				//Move
+			//Disparos Laser
+			for (size_t i = 0; i < player.bullets.size(); i++){
+				//Movimiento
 				player.bullets[i].shape.move(20.f, 0.f);
-
-				//Out of window bounds
-				if (player.bullets[i].shape.getPosition().x > window.getSize().x)
-				{
+				//Limitacion de movimiento
+				if (player.bullets[i].shape.getPosition().x > window.getSize().x){
 					player.bullets.erase(player.bullets.begin() + i);
 					break;
 				}
-
-				//Enemy collision
-				for (size_t k = 0; k < enemies.size(); k++)
-				{
-					if (player.bullets[i].shape.getGlobalBounds().intersects(enemies[k].shape.getGlobalBounds()))
-					{
-						if (enemies[k].HP <= 1)
-						{
+				//Colision con enemigo
+				for (size_t k = 0; k < enemies.size(); k++){
+					if (player.bullets[i].shape.getGlobalBounds().intersects(enemies[k].shape.getGlobalBounds())){
+						if (enemies[k].HP <= 1){
 							score += enemies[k].HPMax;
 							enemies.erase(enemies.begin() + k);
 						}
 						else
 							enemies[k].HP--; //ENEMY TAKE DAMAGE
-
 						player.bullets.erase(player.bullets.begin() + i);
 						break;
 					}
 				}
 			}
 
-			//Enemy
+			//Enmigo
 			if (enemySpawnTimer < 25)
 				enemySpawnTimer++;
 
 			//enemy spawn
-			if (enemySpawnTimer >= 25)
-			{
+			if (enemySpawnTimer >= 25){
 				enemies.push_back(Enemy(&enemyTex, window.getSize()));
 				enemySpawnTimer = 0; //reset timer
 			}
 
-			for (size_t i = 0; i < enemies.size(); i++)
-			{
+			for (size_t i = 0; i < enemies.size(); i++){
 				enemies[i].shape.move(-6.f, 0.f);
 
-				if (enemies[i].shape.getPosition().x <= 0 - enemies[i].shape.getGlobalBounds().width)
-				{
+				if (enemies[i].shape.getPosition().x <= 0 - enemies[i].shape.getGlobalBounds().width){
 					enemies.erase(enemies.begin() + i);
 					break;
 				}
 
-				if (enemies[i].shape.getGlobalBounds().intersects(player.shape.getGlobalBounds()))
-				{
+				if (enemies[i].shape.getGlobalBounds().intersects(player.shape.getGlobalBounds())){
 					enemies.erase(enemies.begin() + i);
-
 					player.HP--; // PLAYER TAKE DAMAGE
+					
 					break;
 				}
 			}
-
 			//UI Update
 			scoreText.setString("Puntaje: " + std::to_string(score));
 		}
 
 		//Draw ===================================================================== DRAW
 		window.clear();
-
 		//player
 		window.draw(player.shape);
-
 		//Bullets
-		for (size_t i = 0; i < player.bullets.size(); i++)
-		{
+		for (size_t i = 0; i < player.bullets.size(); i++){
 			window.draw(player.bullets[i].shape);
 		}
 
 		//enemy
-		for (size_t i = 0; i < enemies.size(); i++)
-		{
+		for (size_t i = 0; i < enemies.size(); i++){
 			eHpText.setString(std::to_string(enemies[i].HP) + "/" + std::to_string(enemies[i].HPMax));
 			eHpText.setPosition(enemies[i].shape.getPosition().x, enemies[i].shape.getPosition().y - eHpText.getGlobalBounds().height);
 			window.draw(eHpText);
@@ -258,12 +221,9 @@ int main()
 		//UI
 		window.draw(scoreText);
 		window.draw(hpText);
-
 		if (player.HP <= 0)
 			window.draw(gameOverText);
-
 		window.display();
 	}
-
 	return 0;
 }
